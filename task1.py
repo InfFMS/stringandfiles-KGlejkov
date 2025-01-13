fcbb4371334b7fbb1a59a3024735779b1e272cb7
Gleb <gklejkov@gmail.com>
2025-01-13 09:11:47 +0700

    Решена первая задача

--- task1.py
+++ task1.py
@@ -4,3 +4,12 @@
 # Общее количество слов во всем тексте файла.
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
+line_count = sum(1 for line in open("task1.txt"))
+print("Строк" , line_count)
+words = 0
+symbols = 0
+for line in open("task1.txt"):
+    words += len(line.split())
+    symbols += len(line.replace(' ', '')) 
+print("Слов" , words)
+print("Символов" , symbols) 
