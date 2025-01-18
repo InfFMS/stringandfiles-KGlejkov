b1d6e2462f5bf4d53f846a4374d94d72106c9ed0
Gleb <gklejkov@gmail.com>
2025-01-18 13:52:00 +0700

    второй номер

--- task2.py
+++ task2.py
@@ -3,3 +3,14 @@
 # Найдите и замените все вхождения слова "Python" на слово "Питон" (регистр учитывать).
 # Запишите обновленный текст в новый файл с другим именем.
 # Выведите на экран сообщение о количестве произведённых замен.
+f = open("task2.txt", "w+", encoding="utf-8")
+fail = f.read()
+print(fail)
+new_fail = fail.replace("Python", "Питон")
+print(new_fail)
+s = fail.count("Питон")
+f.close()
+new_fail1 = open("new_file_for_task_2.txt", "w", encoding="utf-8")
+new_fail1.write(new_fail)
+new_fail1.close()
+
